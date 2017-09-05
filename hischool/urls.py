f3f228c0312fe4739eee6243cc3bd6515d289a54
Clément Aubin <aubincleme@gmail.com>
2017-09-05 18:25:01 +0200

    #6: Remove templates and views from the project
    * Update root URL configuration to add new route to /api
    * Introduce dispatcher application in root URL configuration
    * Introduce displayer application in root URL configuration

--- hischool/urls.py
+++ hischool/urls.py
@@ -1,11 +1,34 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2017 The HiSchool! Project
+#
+# This file is part of HiSchool!.
+#
+# HiSchool! is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# HiSchool! is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with HiSchool!.  If not, see <http://www.gnu.org/licenses/>.
 """HiSchool! URL Configuration."""
 from django.conf.urls import include, url
 from django.contrib import admin
 
+"""
+Redirect every API call the dispatcher application that is responsible for forwarding API queries to the correct
+application
+
+Non-api calls are relayed to a displayer application that injects the URL into the client template
+"""
 urlpatterns = [
-    url(r'^accounts/', include('accounts.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^classes/', include('classes.urls')),
-    url(r'^courses/', include('courses.urls')),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api/', include('dispatcher.urls')),
+    url(r'^', include('displayer.urls'))
 ]
